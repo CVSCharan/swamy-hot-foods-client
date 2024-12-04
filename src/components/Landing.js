e5a742bebb710509a2d522e3858cfee224340397
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2024-12-05 03:08:03 +0530

    Landing Page Info Added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,6 +3,7 @@ import { useShopStatus } from "../context/StatusContext";
 import GoogleReviewsComp from "./GoogleRatingsComp";
 import { useLogo } from "../context/LogoCoontext";
 import { WaveText } from "./TextAnimation";
+import { AccessTime, CalendarToday } from "@mui/icons-material";
 
 const Landing = () => {
   const { shopStatus, cooking } = useShopStatus();
@@ -25,7 +26,7 @@ const Landing = () => {
             --- Pure Veg ---
           </span>
         </div>
-        <h2 className="cinzel-text address-heading">Address:</h2>
+        <h2 className="cinzel-text address-heading">Address</h2>
         <h3 className="quicksand-text address-body">
           7-1-931, Opp. road, nellore railway station west entrance,
         </h3>
@@ -45,6 +46,33 @@ const Landing = () => {
             {shopStatus ? "We are Open! 😊" : "Sorry, We're Closed Now. 😔"}
           </div>
         )}
+
+        <div className="working-hours">
+          <h2>Working Hours</h2>
+          <div className="hours-container">
+            <div className="hours-item">
+              <AccessTime style={{ fontSize: "1.5rem", color: "#FF6347" }} />
+              <span className="quicksand-text time-label">Morning Shift:</span>
+              <span className="josefin-sans-text time">5:30 AM - 11:00 AM</span>
+            </div>
+            <div className="hours-item">
+              <AccessTime sx={{ fontSize: "1.5rem", color: "#FF6347" }} />
+              <span className="quicksand-text time-label">Evening Shift:</span>
+              <span className="josefin-sans-text time">4:30 PM - 9:00 PM</span>
+            </div>
+            <div className="hours-item">
+              <CalendarToday sx={{ fontSize: "1.5rem", color: "#FF6347" }} />
+              <span className="quicksand-text time-label">Holiday:</span>
+              <span className="josefin-sans-text time">Sunday</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="call-us-container">
+          <a href="tel:+91 9642415385" className="cinzel-text call-us-button">
+            <i class="fas fa-phone-alt"></i> Call Us: +91 9642415385
+          </a>
+        </div>
       </div>
       <div className="landing-container-two">
         <GoogleReviewsComp />
