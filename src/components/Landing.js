8d388dff3706b29e1821445089bd47e1cad1a52b
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-01-22 15:46:33 +0530

    Added Shar option on Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useShopStatus } from "../context/StatusContext";
 import { useLogo } from "../context/LogoCoontext";
 import { WaveText } from "./TextAnimation";
-import { AccessTime, CalendarToday } from "@mui/icons-material";
+import { AccessTime, CalendarToday, Share } from "@mui/icons-material";
 import GoogleReviews from "./CutomGoogleRatings";
 import Footer from "./Footer";
 import GetDirectionsButton from "./GMapsDirection";
@@ -11,6 +11,7 @@ import { Typewriter } from "./TypewriterEffect";
 import WhatsAppComp from "./WhatsAppComp";
 import RestaurantMenuOutlinedIcon from "@mui/icons-material/RestaurantMenuOutlined";
 import { Link } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Landing = () => {
   const {
@@ -27,6 +28,24 @@ const Landing = () => {
   useEffect(() => {
     console.log(noticeBoardTxt);
   }, [noticeBoardTxt]);
+
+  // Function to handle sharing
+  const handleShare = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: "Swamy's Hot Foods",
+          text: "Check out this amazing vegetarian restaurant in Nellore!",
+          url: "https://swamyshotfoods.shop",
+        });
+        console.log("Shared successfully");
+      } catch (error) {
+        console.error("Error sharing", error);
+      }
+    } else {
+      alert("Your browser does not support the Web Share API.");
+    }
+  };
 
   return (
     <main id="Landing" className="App">
@@ -166,14 +185,49 @@ const Landing = () => {
           <a
             href="tel:+91 9642415385"
             className="josefin-sans-text call-us-button"
+            style={{ borderColor: "#FF7722", color: "#FF7722" }}
           >
-            <i className="fa-solid fa-phone"></i> +91 9642415385
+            <div className="call-animation">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="22"
+                viewBox="0 0 122.9 98.9"
+                fill="#FF7722"
+              >
+                <path
+                  d="M109,98.9H13.7v-14c0.5-16.3,14.9-28.7,23.6-42.8V29.7h14.5v8.9h19.5v-8.9h14.5v12.4C95.2,57.2,109,67,109,84.7V98.9z 
+           M122.5,42.1c0-2.2,0.4-4.4,0.1-6.8c-10.7,3.5-21.1,2.5-31.3-3.1c-0.4,3.8,0.2,7.2,1.6,10.4C96.5,50.6,122.2,53.3,122.5,42.1z 
+           M0.3,42.1c0-2.2-0.4-4.4-0.1-6.8c10.7,3.5,21.1,2.5,31.3-3.1c0.4,3.8-0.2,7.2-1.6,10.4C26.3,50.6,0.5,53.3,0.3,42.1z 
+           M0,31.9C8.6-8.2,115.4-13,122.9,32c-10.4,2.9-21,1.2-31.6-3.6c0.3-2.1-0.2-3.8-1.3-5.2c-6.3-7.9-51.4-8.2-57.2,0.3
+           c-0.9,1.3-1.3,2.9-1.2,4.7C21.1,34.6,10.5,36.1,0,31.9z 
+           M47.2,47.7c2.4,0,4.3,1.9,4.3,4.3s-1.9,4.3-4.3,4.3-4.3-1.9-4.3-4.3,1.9-4.3,4.3-4.3z 
+           M74.8,71.7c2.4,0,4.3,1.9,4.3,4.3s-1.9,4.3-4.3,4.3-4.3-1.9-4.3-4.3,1.9-4.3,4.3-4.3z 
+           M61,71.7c2.4,0,4.3,1.9,4.3,4.3s-1.9,4.3-4.3-4.3-4.3-1.9-4.3-4.3,1.9-4.3,4.3-4.3z 
+           M47.2,71.7c2.4,0,4.3,1.9,4.3,4.3s-1.9,4.3-4.3,4.3-4.3-1.9-4.3-4.3,1.9-4.3,4.3-4.3z"
+                />
+              </svg>
+            </div>
+            Call
           </a>
+          {/* Share Button */}
+          <button
+            onClick={handleShare}
+            className="josefin-sans-text call-us-button share-button"
+            style={{ borderColor: "#4CAF50", color: "#4CAF50" }}
+          >
+            <i class="fa-solid fa-share" style={{ color: "#4CAF50" }}></i> Share
+          </button>
+
           <Link
             to="/menu"
             className="josefin-sans-text call-us-button"
+            style={{ color: "royalblue" }}
           >
-            <RestaurantMenuOutlinedIcon /> Menu
+            <RestaurantMenuOutlinedIcon
+              sx={{ fontSize: 22, fill: "royalblue" }}
+            />{" "}
+            Menu
           </Link>
         </div>
 
